945ca9868992cf22b1ee726c6c404b504975a579
Roberto Arruda <betoakang@hotmail.com>
2022-05-13 00:18:29 -0300

    chore: add jest configs for mongodb

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**', '!<rootDir>/src/**/*-protocols.ts', '!**/protocols/**', '!**/test/**'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
+  preset: '@shelf/jest-mongodb',
   coverageProvider: 'babel',
   transform: {
     '.+\\.ts': 'ts-jest'
