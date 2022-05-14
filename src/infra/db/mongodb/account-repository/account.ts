e13b9d02c3256c2a8eda5db89fd114e73016332e
Roberto Arruda <betoakang@hotmail.com>
2022-05-13 21:39:46 -0300

    feat: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,13 @@
+import { AddAccountRepository } from '../../../../data/protocols/add-account-repository'
+import { AddAccountModel } from '../../../../domain/usecases/add-account'
+import { AccountModel } from '../../../../domain/models/account'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add (accountData: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getColletion('accounts')
+    const result = await accountCollection.insertOne(accountData)
+    const account = { id: result.insertedId.toString(), ...accountData }
+    return account
+  }
+}
