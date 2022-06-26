c0160f4241b6bf27c31f6c79d9b644618969f4df
Roberto Arruda <betoakang@hotmail.com>
2022-06-26 17:52:09 -0300

    feat: ensure DbSaveSurveyResult return a survey result on success

--- src/data/usecases/save-survey-result/db-save-survey-result.ts
+++ src/data/usecases/save-survey-result/db-save-survey-result.ts
@@ -6,7 +6,7 @@ export class DbSaveSurveyResult implements ISaveSurveyResult {
   ) {}
 
   async save (data: SaveSurveyResultModel): Promise<SurveyResultModel> {
-    await this.saveSurveyResultRepository.save(data)
-    return null
+    const surveyResult = await this.saveSurveyResultRepository.save(data)
+    return surveyResult
   }
 }
