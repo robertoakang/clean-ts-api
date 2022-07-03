1da1fd204512b9a686768576b775e820c2c1a4b7
Roberto Arruda <betoakang@hotmail.com>
2022-07-03 19:54:05 -0300

    docs: update info from header

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -7,23 +7,30 @@ export default {
   info: {
     title: 'CLEAN TS API',
     description: 'API do curso do Mango para realizar enquetes entre programadores',
-    version: '1.0.0'
+    version: '1.0.0',
+    contact: {
+      name: 'Roberto Kang',
+      url: 'https://www.linkedin.com/in/robertoakang'
+    },
+    license: {
+      name: 'GPL-3.0-or-later',
+      url: 'https://spdx.org/licenses/GPL-3.0-or-later.html'
+    }
   },
-  license: {
-    name: 'GPL-3.0-or-later',
-    url: 'https://spdx.org/licenses/GPL-3.0-or-later.html'
-  },
-  contact: {
-    name: 'Roberto Kang',
-    url: 'https://robertoakang.github.io/'
+  externalDocs: {
+    description: 'Link para o treinamento completo do Rodrigo Manguinho',
+    url: 'https://www.udemy.com/share/102x5i3@7iqnKrTJ3s_6f1xryghpRe0STehMNy4FmxgpdgGiuLvCMHTzxcJfgIqPT7y_rpw3/'
   },
   servers: [{
-    url: '/api'
+    url: '/api',
+    description: 'Servidor Principal'
   }],
   tags: [{
-    name: 'Login'
+    name: 'Login',
+    description: 'APIs relacionadas a Login'
   }, {
-    name: 'Enquetes'
+    name: 'Enquetes',
+    description: 'APIs relacionadas a Enquete'
   }],
   paths,
   schemas,
