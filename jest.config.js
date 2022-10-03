68f249b805ef0828f99bb0d8813dd054da8270c4
Roberto Kang <roberto.kang@bolttech.io>
2022-10-03 18:35:07 -0300

    fix: correct implementation uuid

--- jest.config.js
+++ jest.config.js
@@ -2,15 +2,19 @@ module.exports = {
   roots: ['<rootDir>/tests'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
-    '!<rootDir>/src/main/**'],
+    '!<rootDir>/src/main/**'
+  ],
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
   testEnvironment: 'node',
   preset: '@shelf/jest-mongodb',
   transform: {
-    '.+\\.ts': 'ts-jest'
+    '.+\\.ts$': 'ts-jest'
   },
+  transformIgnorePatterns: ['<rootDir>/node_modules/.+.(js|jsx)$'],
   moduleNameMapper: {
-    '@/(.*)': '<rootDir>/src/$1'
+    '@/tests/(.*)': '<rootDir>/tests/$1',
+    '@/(.*)': '<rootDir>/src/$1',
+    uuid: require.resolve('uuid')
   }
 }
