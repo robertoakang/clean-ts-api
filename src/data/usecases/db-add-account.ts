08670f8b969835fdb03de4f96c439df23761881c
Roberto Kang <roberto.kang@bolttech.io>
2022-10-03 14:54:54 -0300

    feat: add ICheckAccountByEmailRepository

--- src/data/usecases/db-add-account.ts
+++ src/data/usecases/db-add-account.ts
@@ -1,18 +1,18 @@
-import { IAddAccountRepository, IHasher, ILoadAccountByEmailRepository } from '@/data/protocols'
+import { IAddAccountRepository, ICheckAccountByEmailRepository, IHasher } from '@/data/protocols'
 import { IAddAccount } from '@/domain/usecases'
 
 export class DbAddAccount implements IAddAccount {
   constructor (
     private readonly hasher: IHasher,
     private readonly addAccountRepository: IAddAccountRepository,
-    private readonly loadAccountByEmailRepository: ILoadAccountByEmailRepository
+    private readonly checkAccountByEmailRepository: ICheckAccountByEmailRepository
 
   ) {}
 
   async add (accountData: IAddAccount.Params): Promise<IAddAccount.Result> {
-    const account = await this.loadAccountByEmailRepository.loadByEmail(accountData.email)
+    const exists = await this.checkAccountByEmailRepository.checkByEmail(accountData.email)
     let isValid = false
-    if (!account) {
+    if (!exists) {
       const hashedPassword = await this.hasher.hash(accountData.password)
       isValid = await this.addAccountRepository.add({ ...accountData, password: hashedPassword })
     }
--- src/infra/db/mongodb/account-mongo-repository.ts
+++ src/infra/db/mongodb/account-mongo-repository.ts
@@ -1,8 +1,8 @@
-import { IAddAccountRepository, ILoadAccountByEmailRepository, ILoadAccountByTokenRepository, IUpdateAccessTokenRepository } from '@/data/protocols'
+import { IAddAccountRepository, ICheckAccountByEmailRepository, ILoadAccountByEmailRepository, ILoadAccountByTokenRepository, IUpdateAccessTokenRepository } from '@/data/protocols'
 import { MongoHelper } from '@/infra/db'
 import { ObjectId } from 'mongodb'
 
-export class AccountMongoRepository implements IAddAccountRepository, ILoadAccountByEmailRepository, IUpdateAccessTokenRepository, ILoadAccountByTokenRepository {
+export class AccountMongoRepository implements IAddAccountRepository, ILoadAccountByEmailRepository, IUpdateAccessTokenRepository, ILoadAccountByTokenRepository, ICheckAccountByEmailRepository {
   async add (data: IAddAccountRepository.Params): Promise<IAddAccountRepository.Result> {
     const accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.insertOne(data)
@@ -21,6 +21,18 @@ export class AccountMongoRepository implements IAddAccountRepository, ILoadAccou
       }
     })
     return account && MongoHelper.map(account)
+  }
+
+  async checkByEmail (email: string): Promise<ICheckAccountByEmailRepository.Result> {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    const account = await accountCollection.findOne({
+      email
+    }, {
+      projection: {
+        _id: 1
+      }
+    })
+    return account !== null
   }
 
   async updateAccessToken (id: string, token: string): Promise<void> {
