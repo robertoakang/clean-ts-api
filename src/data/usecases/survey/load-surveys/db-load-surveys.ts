855d073e46118efadaad60dc25a1849d815276d3
Roberto Kang <roberto.kang@bolttech.io>
2022-10-03 07:39:23 -0300

    feat: change load surveys to add didAnswer field

--- src/data/usecases/survey/load-surveys/db-load-surveys.ts
+++ src/data/usecases/survey/load-surveys/db-load-surveys.ts
@@ -3,8 +3,8 @@ import { ILoadSurveysRepository, ILoadSurveys, SurveyModel } from './db-load-sur
 export class DbLoadSurveys implements ILoadSurveys {
   constructor (private readonly loadSurveysRepository: ILoadSurveysRepository) {}
 
-  async load (): Promise<SurveyModel[]> {
-    const surveys = await this.loadSurveysRepository.loadAll()
+  async load (accountId: string): Promise<SurveyModel[]> {
+    const surveys = await this.loadSurveysRepository.loadAll(accountId)
     return surveys
   }
 }
