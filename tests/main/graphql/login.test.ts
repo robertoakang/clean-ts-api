81d2b7af124a82b21c0a3bc1c2599c7dd2ac2919
Roberto Kang <roberto.kang@bolttech.io>
2022-10-04 14:41:33 -0300

    test: ensure Login Query returns an UnauthorizedError on invalid credentials

--- tests/main/graphql/login.test.ts
+++ tests/main/graphql/login.test.ts
@@ -40,10 +40,18 @@ describe('Login GraphQL', () => {
       const res = await request(app)
         .post('/graphql')
         .send({ query })
-
       expect(res.status).toBe(200)
       expect(res.body.data?.login.accessToken).toBeTruthy()
       expect(res.body.data?.login.name).toBe('Roberto')
+    })
+
+    test('Should return UnauthorizedError on invalid credentials', async () => {
+      const res = await request(app)
+        .post('/graphql')
+        .send({ query })
+      expect(res.status).toBe(401)
+      expect(res.body.data).toBeFalsy()
+      expect(res.body.errors[0].message).toBe('Unauthorized')
     })
   })
 })
